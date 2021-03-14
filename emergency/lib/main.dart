b95988635f7acecbb1f9600f1892207f2e3cb3e9
Astha Nayak <nayakastha2911@gmail.com>
2021-03-14 22:46:12 +0530

    locator

--- emergency/lib/main.dart
+++ emergency/lib/main.dart
@@ -1,8 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:splash/splash.dart';
+import 'package:geocoder/geocoder.dart';
+import 'package:location/location.dart';
+import 'package:url_launcher/url_launcher.dart' as UrlLauncher;
 
 void main() {
   runApp(MyApp());
+}
+
+getLocation(double lat, double lon) async {
+  Location location = new Location();
+
+  bool _serviceEnabled;
+  PermissionStatus _permissionGranted;
+  LocationData _locationData;
+
+  _serviceEnabled = await location.serviceEnabled();
+  if (!_serviceEnabled) {
+    _serviceEnabled = await location.requestService();
+    if (!_serviceEnabled) {
+      return;
+    }
+  }
+
+  _permissionGranted = await location.hasPermission();
+  if (_permissionGranted == PermissionStatus.denied) {
+    _permissionGranted = await location.requestPermission();
+    if (_permissionGranted != PermissionStatus.granted) {
+      return;
+    }
+  }
+
+  _locationData = await location.getLocation();
+  lat = _locationData.latitude;
+  lon = _locationData.longitude;
+  final coordinate = new Coordinates(lat, lon);
+  List<dynamic> address =
+      await Geocoder.local.findAddressesFromCoordinates(coordinate);
+  var first = address.first;
 }
 
 class MyApp extends StatefulWidget {
@@ -27,6 +61,9 @@ class _MyAppState extends State<MyApp> {
         backgroundColor: Color.fromRGBO(45, 48, 45, 0.8),
         body: Center(
           child: InkWell(
+            onTap: () {
+              UrlLauncher.launch('sms://+919437255618');
+            },
             child: Stack(
               fit: StackFit.loose,
               children: [
